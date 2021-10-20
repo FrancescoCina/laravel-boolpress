66a4bd26626eac3be507aec38ec5193a84ec24c8
FrancescoCina <francesco.cina04@gmail.com>
2021-10-20 20:22:20 +0200

    Trasformato a componente il pageNavigator

---
+++ resources/js/components/PageNavigator.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page-navigator mt-5">
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item">
+          <a
+            v-if="pagePosts > 1"
+            class="page-link"
+            @click="getAllPostsFromApi(pagePosts - 1)"
+            >Previous</a
+          >
+        </li>
+        <li
+          v-for="i in 5"
+          :key="i"
+          class="page-item"
+          :class="pagePosts === i ? 'active' : ''"
+        >
+          <a @click="getAllPostsFromApi((pagePosts = i))" class="page-link">{{
+            i
+          }}</a>
+        </li>
+        <li v-if="pagePosts !== lastPagePosts" class="page-item">
+          <a class="page-link" @click="getAllPostsFromApi(pagePosts + 1)"
+            >Next</a
+          >
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNavigator",
+  props: ["pagePosts", "lastPagePosts"],
+};
+</script>
+
+<style>
+</style>
--- resources/js/components/posts/Posts.vue
+++ resources/js/components/posts/Posts.vue
@@ -11,37 +11,8 @@
     <div v-else>
       <h2>La lista dei posts</h2>
       <CardPost v-for="post in posts" :key="post.id" :post="post" />
-      <div class="page-navigator mt-5">
-        <nav aria-label="Page navigation example">
-          <ul class="pagination">
-            <li class="page-item">
-              <a
-                v-if="pagePosts > 1"
-                class="page-link"
-                @click="getAllPostsFromApi(pagePosts - 1)"
-                >Previous</a
-              >
-            </li>
-            <li
-              v-for="i in 5"
-              :key="i"
-              class="page-item"
-              :class="pagePosts === i ? 'active' : ''"
-            >
-              <a
-                @click="getAllPostsFromApi((pagePosts = i))"
-                class="page-link"
-                >{{ i }}</a
-              >
-            </li>
-            <li v-if="pagePosts !== lastPagePosts" class="page-item">
-              <a class="page-link" @click="getAllPostsFromApi(pagePosts + 1)"
-                >Next</a
-              >
-            </li>
-          </ul>
-        </nav>
-      </div>
+
+      <PageNavigator :pagePosts="pagePosts" :lastPagePosts="lastPagePosts" />
     </div>
   </section>
 </template>
@@ -49,12 +20,14 @@
 <script>
 import CardPost from "./CardPost.vue";
 import Loader from "../Loader.vue";
+import PageNavigator from "../PageNavigator.vue";
 
 export default {
   name: "Posts",
   components: {
     CardPost,
     Loader,
+    PageNavigator,
   },
   data() {
     return {
