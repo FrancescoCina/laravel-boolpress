4e3a7355428cbde64f9f75e913d73e76ae68dfd0
FrancescoCina <francesco.cina04@gmail.com>
2021-10-22 18:35:28 +0200

    Aggiunto attraverso seeder 3 utenti alla tabella users/ inseriti in ogni post un user_id refreshando database e richiamando tutti i seeders

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -5,6 +5,7 @@ use App\Models\Post;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Category;
+use App\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
@@ -17,14 +18,13 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-
         $categories_id = Category::select('id')->pluck('id')->toArray();
-
-
+        $users_id = User::select('id')->pluck('id')->toArray();
         for ($i = 0; $i < 50; $i++) {
             $post = new Post();
             $post->title = $faker->words(3, true);
             $post->category_id = Arr::random($categories_id);
+            $post->user_id = Arr::random($users_id);
             $post->content = $faker->text(2000);
             $post->image = $faker->imageUrl(250, 250);
             $post->slug = Str::slug($post->title, '-');
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use PharIo\Manifest\Email;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'Francesco',
+                'email' => 'francesco@gmail.com',
+                'password' => 'ciaociaociao'
+            ],
+            [
+                'name' => 'Giancarlo',
+                'email' => 'giancarlo@gmail.com',
+                'password' => 'ciaociaociao'
+            ],
+            [
+                'name' => 'Giuseppe',
+                'email' => 'giuseppe@gmail.com',
+                'password' => 'ciaociaociao'
+            ],
+            [
+                'name' => 'Amedeo',
+                'email' => 'amedeo@gmail.com',
+                'password' => 'ciaociaociao'
+            ],
+
+        ];
+
+        foreach ($users as $utente) {
+            $user = new User();
+            $user->name = $utente['name'];
+            $user->email = $utente['email'];
+            $user->password = $utente['password'];
+            $user->save();
+        }
+    }
+}
