7a4d35ed3febe96b6018f992a780827d7dc5634e
FrancescoCina <francesco.cina04@gmail.com>
2021-10-22 13:05:44 +0200

    Stampato autore anche nella Show dei singoli post

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,6 +6,7 @@
     <a href="{{ route('admin.posts.index') }}" class="btn btn-outline-secondary">Indietro</a>
     <div class="mt-3">
         <h3>{{$post->title}}</h3>
+        <small>@if($post->user){{ $post->user->name }} @else Anonimo @endif</small>
         <p>{{ $post->content }}</p>
         <img src="{{ $post->image }}" alt="{{ $post->title }}">
     </div>
